98c42aa0d2e87d86a3ea15d2ea514d2b1cd0a40f
Shaojie Jiang <shaojie.jiang1@gmail.com>
2025-03-28 08:55:32 +0100

    Fix page_state definition and initialisation

--- aic_core/streamlit/agent_page.py
+++ aic_core/streamlit/agent_page.py
@@ -12,33 +12,40 @@ from aic_core.agent.agent import AgentConfig
 from aic_core.agent.agent import AgentFactory
 from aic_core.streamlit.mixins import AgentSelectorMixin
 from aic_core.streamlit.page import AICPage
-from aic_core.streamlit.page import app_state_registry
+from aic_core.streamlit.page import app_state
 
 
+@app_state(__file__)
 class PageState:
-    """Page state."""
+    """Page state.
+    
+    Acting as a template here. Must be defined in the page that uses it.
+    """
 
     chat_history: list[ModelMessage] = []
 
 
 class AgentPage(AICPage, AgentSelectorMixin):
-    """Chatbot page."""
+    """Agent page.
+
+    PageState needs to have the following values:
+    - chat_history: list[ModelMessage]
+    """
 
     def __init__(
-        self, repo_id: str, page_title: str = "Agent"
+        self, repo_id: str, page_state: PageState, page_title: str = "Agent"
     ) -> None:
         """Initialize the page."""
         super().__init__()
         self.repo_id = repo_id
         self.page_title = page_title
+        self.page_state = page_state
         self.user_role = "user"
         self.assistant_role = "assistant"
-        if "chat_history" not in st.session_state:
-            st.session_state["chat_history"] = []
 
     def reset_chat_history(self) -> None:
         """Reset chat history."""
-        st.session_state["chat_history"] = []
+        self.page_state.chat_history = []
 
     def get_agent(self, agent_name: str) -> Agent:
         """Get agent."""
@@ -50,14 +57,14 @@ class AgentPage(AICPage, AgentSelectorMixin):
 
     async def get_response(self, user_input: str, agent: Agent) -> None:
         """Get response from agent."""
-        history = st.session_state["chat_history"]
+        history = self.page_state.chat_history
         st.chat_message(self.user_role).write(user_input)
         if agent._mcp_servers:
             async with agent.run_mcp_servers():
                 result = await agent.run(user_input, message_history=history)  # type: ignore
         else:
             result = await agent.run(user_input, message_history=history)  # type: ignore
-        st.session_state["chat_history"].extend(result.new_messages())
+        self.page_state.chat_history.extend(result.new_messages())
 
     def to_simple_messages(
         self, msg_parts: list[ModelRequestPart] | list[ModelResponsePart]
@@ -77,7 +84,7 @@ class AgentPage(AICPage, AgentSelectorMixin):
 
     def display_chat_history(self) -> None:
         """Display chat history."""
-        for msg in st.session_state["chat_history"]:
+        for msg in self.page_state.chat_history:
             simp_msgs = self.to_simple_messages(msg.parts)
             for simp_msg in simp_msgs:
                 st.chat_message(simp_msg[0]).write(simp_msg[1])
